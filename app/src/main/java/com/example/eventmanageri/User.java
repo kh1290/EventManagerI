6f0f9192aaf3be89f0f492afe28876baaa9b4978
kh1290 <kahye1290@gmail.com>
2019-11-12 01:26:44 +0100

    - Update/Delete an event - 10 Nov
    - Add 'Users' database
    - Compare UID when listing events
    - 11 Nov

---
+++ app/src/main/java/com/example/eventmanageri/User.java
@@ -0,0 +1,51 @@
+package com.example.eventmanageri;
+
+public class User {
+    private String key;
+    private String email;
+    private String role;
+    private String uid;
+
+    public User() {
+    }
+
+    public User(String key, String email, String role, String uid) {
+        this.key = key;
+        this.email = email;
+        this.role = role;
+        this.uid = uid;
+    }
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
